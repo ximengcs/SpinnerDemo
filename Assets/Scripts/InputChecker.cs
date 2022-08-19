c079c4b3ec48d94c469a05bbd54a20beb180bc07
ximengcs <ximengcs@163.com>
2022-08-19 19:25:11 +0800

    commit spinner demo

---
+++ Assets/Scripts/InputChecker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class InputChecker : MonoBehaviour {
+    public RotateController m_Target;
+
+    private const float GAP = 0.1f;
+    private const float MAXFORCE = 30;
+    private float m_Time = 0.1f;
+    private Vector2 m_Point;
+
+    private void Update() {
+        if(Input.GetMouseButton(0) || Input.GetMouseButtonDown(0)) {
+            if(m_Time <= 0) {
+                float force = Input.mousePosition.x - m_Point.x;
+                To(force * 0.1f);
+                m_Time = GAP;
+                m_Point = Input.mousePosition;
+            }
+            else
+                m_Time -= Time.deltaTime;
+        } else if(Input.GetMouseButtonUp(0)) {
+            To(Input.mousePosition.x - m_Point.x + m_Target.Velocity);
+            m_Time = GAP;
+        }
+    }
+
+    private void To(float force) {
+        if(force > 0)
+            force = Mathf.Min(force, MAXFORCE);
+        else
+            force = Mathf.Max(force, -MAXFORCE);
+        m_Target.Velocity = force;
+    }
+}
---
+++ Assets/Scripts/RotateController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateController : MonoBehaviour
+{
+    private float m_Velocity;
+    private float m_Max;
+
+    public Text m_UI;
+
+    public float Velocity {
+        get { return m_Velocity; }
+        set {
+            m_Velocity = value;
+            m_Max = value;
+        }
+    }
+
+    private void Update() {
+        if(!Mathf.Approximately(m_Velocity, 0f)) {
+            Rotate();
+            float inchValue = InchingFun(1 - m_Velocity / m_Max);
+            bool plus = m_Velocity > 0;
+            if(plus)
+                m_Velocity -= inchValue;
+            else
+                m_Velocity += inchValue;
+
+            if(m_Velocity > 0 != plus)
+                m_Velocity = 0;
+        } else {
+            m_Velocity = 0;
+        }
+    }
+
+    private void FixedUpdate() {
+        float speed = 60 / 0.02f * m_Velocity / 360;
+        m_UI.text = Mathf.Abs((int)speed) + "/min";
+    }
+
+    private void Rotate() {
+        transform.Rotate(Vector3.forward, m_Velocity, Space.Self);
+    }
+
+    private float InchingFun(float pro) {
+        //return (4 - time * time) * 0.04f;
+        return Mathf.Pow(0.003f, pro);
+    }
+}
